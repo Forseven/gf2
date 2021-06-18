53b2f2de9b41dd89ec2d67c06628d16b957e16e7
fors <nutaru@gmail.com>
2021-06-18 23:36:14 +0700

    lesson1

--- mainapp/views.py
+++ mainapp/views.py
@@ -76,7 +76,7 @@ def products(request, pk=None, page=1):
             'links_menu': links_menu,
             'category': category,
             'products': products_paginator,
-            'basket': basket,
+
         }
         
         return render(request, 'mainapp/products_list.html', content)
@@ -89,7 +89,7 @@ def products(request, pk=None, page=1):
         'links_menu': links_menu, 
         'hot_product': hot_product,
         'same_products': same_products,
-        'basket': basket,
+
     }
     
     return render(request, 'mainapp/products.html', content)
